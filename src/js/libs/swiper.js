4a88a35f3d4430d21e06b7b132ccf56efc17d778
Artem Lizantan 2023 <plishkan22@gmail.com>
2023-06-16 15:06:48 +0300

    stock section

--- src/js/libs/swiper.js
+++ src/js/libs/swiper.js
@@ -1,22 +1,22 @@
 import Swiper from "swiper/bundle";
 
 function swiperHero() {
-  const swiper = new Swiper(".swiper", {
+  const swiper = new Swiper(".hero__swiper", {
     slidesPerView: 1,
     spaceBetween: 10,
     on: {
       slideChange: function () {
         const activeSlide = this.realIndex + 1;
         console.log("Активный слайд:", activeSlide);
-        const slideSpan = document.querySelectorAll(".pagination__count")
-        slideSpan.forEach(activeSpan => {
-            const numberSpan = +activeSpan.textContent;
-            if(activeSlide === numberSpan){
-                activeSpan.classList.add("_active")
-            }else{
-                activeSpan.classList.remove("_active")
-            }
-        })
+        const slideSpan = document.querySelectorAll(".pagination__count");
+        slideSpan.forEach((activeSpan) => {
+          const numberSpan = +activeSpan.textContent;
+          if (activeSlide === numberSpan) {
+            activeSpan.classList.add("_active");
+          } else {
+            activeSpan.classList.remove("_active");
+          }
+        });
       },
     },
   });
